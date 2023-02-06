e63a113a6bdbf6dff42bbdf9e797920799edc487
lashap28 <92382804+lashap28@users.noreply.github.com>
2023-02-06 17:28:37 +0400

    Update settings.py

--- ajaxdemo/settings.py
+++ ajaxdemo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xi!-q4$n6%c+i0%d$a+^sw8v#sh=q=(4^onhm7rh#+g&_gwv4n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
